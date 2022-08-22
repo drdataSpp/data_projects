86ec08eddd6f3685c117bd3f949cb72f4446b583
SOORYA PRAKASH PARTHIBAN <sooryaprakml@gmail.com>
2022-08-22 20:36:24 +1200

    Answered Q.11 to Q.15

--- SQL/SQL Practice Problems/answers.sql
+++ SQL/SQL Practice Problems/answers.sql
@@ -84,3 +84,43 @@ Order the results by BirthDate, so we have the oldest employees first. */
 SELECT [FirstName], [LastName], [Title], [BirthDate]
 FROM [dbo].[Employees]
 ORDER BY 4;
+
+/* 11. Showing only the Date with a DateTime field
+In the output of the query above, showing the Employees in order of BirthDate, we see the time of
+the BirthDate field, which we don’t want. Show only the date portion of the BirthDate field. */
+
+SELECT [FirstName], [LastName], [Title], CAST([BirthDate] AS DATE)
+FROM [dbo].[Employees]
+WHERE [Title] = 'Sales Representative'
+ORDER BY [BirthDate];
+
+/* 12. Employees full name
+Show the FirstName and LastName columns from the Employees table, 
+and then create a new column called FullName, showing FirstName and LastName
+joined together in one column, with a space in between.
+ */
+
+ SELECT [FirstName], [LastName], CONCAT([FirstName],' ' ,[LastName]) AS FullName
+ FROM [dbo].[Employees];
+
+ /* 13. OrderDetails amount per line item 
+ In the OrderDetails table, we have the fields UnitPrice and Quantity. 
+ Create a new field, TotalPrice, that multiplies these two together. We’ll
+ignore the Discount field for now. In addition, show the OrderID, ProductID, UnitPrice,
+and Quantity. Order by OrderID and ProductID */
+
+SELECT [OrderID], [ProductID], [UnitPrice], [Quantity], ([UnitPrice] * [Quantity]) AS TotalPrice
+FROM [dbo].[Order Details]
+ORDER BY 1, 2;
+
+/* 14. How many customers?
+How many customers do we have in the Customers table? 
+Show one value only, and don’t rely on getting the recordcount at the end of a resultset */
+
+SELECT COUNT(DISTINCT [CustomerID]) AS TOTAL_CUSTOMERS
+FROM [dbo].[Customers];
+
+/* 15. When was the first order?
+Show the date of the first order ever made in the Orders table */
+SELECT MIN([OrderDate]) AS FIRST_ORDER
+FROM [dbo].[Orders];
