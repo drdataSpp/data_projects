fc9f78887ce5725e440e6afa3304b6d49ea1db5e
SOORYA PRAKASH PARTHIBAN <sooryaprakml@gmail.com>
2023-05-28 00:44:34 +1200

    Py ETL: Adding if file exists code

--- Python - CSV to MS SQL DB ETL/ETL Py Script/etl-CSV-to-SQL-Server-DB.py
+++ Python - CSV to MS SQL DB ETL/ETL Py Script/etl-CSV-to-SQL-Server-DB.py
@@ -8,21 +8,48 @@
 
 ## Change history
 
-## V1: Pushed draft files with python libraries 
+## V1: Pushed draft files with python libraries
+## V2: Added python code to check if the source csv data file exists and parameter files & folders 
 
 #######################################################################################
 
 ## Importing python libraries
+import sys
+import os.path
 import numpy as np
 import pandas as pd
 import pyodbc as pySQLConn
 
-## Importing Raw CSV file and Pre-Processing
+## Defining Project Path
+PROJECT_PARENT_PATH = "D:/001_Data/data_projects/data_projects/Python - CSV to MS SQL DB ETL"
 
-raw_df = pd.read_csv(r"D:\001_Data\data_projects\data_projects\Python - CSV to MS SQL DB ETL\raw data\Credit Score Classification Dataset.csv")
+## Check if source data file exists
+    ### Getting File name & File Path from a parameter set (With the help of a paramater file, we can make changes easily without editing the actual source code)
 
-## Checking the import
-print(raw_df.head())
+with open(PROJECT_PARENT_PATH + "/Parameter Set/PS_CreditScore.txt") as f:
+    lines = f.read().splitlines() 
 
-## Checking the header count of the csv source file
-print(raw_df[1:0]) 
+    ## Variables for file path, file name and file header
+    FILE_PATH           = str(lines[0:1]).split("=")[1].replace("']", "").strip()
+    FILE_NAME           = str(lines[1:2]).split("=")[1].replace("']", "").strip()
+    FILE_HEADER_NAME    = str(lines[2:3]).split("=")[1].replace("']", "").strip()
+
+print(PROJECT_PARENT_PATH + "/" + FILE_PATH + "/" + FILE_NAME)
+ 
+isSourceDataExists = os.path.exists(PROJECT_PARENT_PATH + "/" + FILE_PATH + "/" + FILE_NAME)
+
+if isSourceDataExists == True:
+    print("Source file exists")
+else:
+    print("Source file does not exists. Program is terminating")
+    sys.exit()
+    
+# ## Importing Raw CSV file and Pre-Processing
+
+# raw_df = pd.read_csv(r"D:\001_Data\data_projects\data_projects\Python - CSV to MS SQL DB ETL\raw data\Credit Score Classification Dataset.csv")
+
+# ## Checking the import
+# print(raw_df.head())
+
+# ## Checking the header count of the csv source file
+# print(raw_df[1:0]) 
