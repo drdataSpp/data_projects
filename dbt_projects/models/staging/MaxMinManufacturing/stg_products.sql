5c1a148df70be5acdfdc386d21816756be854231
SOORYA PRAKASH PARTHIBAN <sooryaprakml@gmail.com>
2024-01-14 04:25:25 +1300

    SuperStore Dimensional modelling

--- dbt_projects/models/staging/MaxMinManufacturing/stg_products.sql
+++ dbt_projects/models/staging/MaxMinManufacturing/stg_products.sql
@@ -1,8 +1,13 @@
 with cte_products as 
 (
-    select ProductCode as product_id,
-     ProductName
-    from {{ source('max_min_sales', 'DimProduct')}}
+    select 
+        Product_ID                              as PRODUCT_ID_NK,
+        CAST(Product_Name AS VARCHAR(500))      as PRODUCT_NAME,
+        Category                                as PRODUCT_CATEGORY,
+        Sub_Category                            as PRODUCT_SUB_CATEGORY
+    from {{ source('RAW_SuperStore_tbl', 'RAW_SuperStore')}}
+    --adding group by clause to filter dups
+    group by Product_ID, Product_Name, Category, Sub_Category
 )
 
 select * from cte_products;
